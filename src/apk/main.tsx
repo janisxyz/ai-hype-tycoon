e9c32d93088e391adf137b7a4e0269e06136739f
janisxyz <janis.schelling@bbzbl-it.ch>
2026-09-07 22:35:39 -0700

    Load Hotel Tycoon theme on top of the APK entry

--- src/apk/main.tsx
+++ src/apk/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { GameApp } from "@/components/game/GameApp";
 import "@/styles.css";
+import "@/hotel.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
